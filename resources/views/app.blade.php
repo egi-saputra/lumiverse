6325e394796a921e30b1f5082c2241b83fd06cd5
egi-saputra <hanalikaproject@gmail.com>
2026-07-09 12:51:58 +0700

    P

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,8 +33,8 @@
         @inertia
 
         <!-- Sandbox -->
-        {{-- <script src="https://app.sandbox.midtrans.com/snap/snap.js" --}}
-        <script src="https://app.midtrans.com/snap/snap.js"
+        <script src="https://app.sandbox.midtrans.com/snap/snap.js"
+        {{-- <script src="https://app.midtrans.com/snap/snap.js" --}}
                 data-client-key="{{ config('midtrans.client_key') }}"></script>
         <!-- Production: ganti URL ke https://app.midtrans.com/snap/snap.js -->
     </body>
