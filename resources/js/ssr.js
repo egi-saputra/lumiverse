1bc80e48aefde57218f5bcfaccf6f369c4b95861
egi-saputra <hanalikaproject@gmail.com>
2026-07-13 20:43:13 +0700

    p

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -4,9 +4,21 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import createServer from "@inertiajs/vue3/server";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import { Ziggy } from "./ziggy.js";
 
-createServer((page) =>
-    createInertiaApp({
+const SSR_ALLOWED = ["Home/School", "Home/Workspace"];
+
+createServer((page) => {
+    // Halaman selain landing page: skip render Vue di server,
+    // biarkan client hydrate normal (CSR biasa).
+    if (!SSR_ALLOWED.includes(page.component)) {
+        return Promise.resolve({
+            head: [],
+            body: `<div id="app" data-page='${JSON.stringify(page).replace(/'/g, "&#39;")}'></div>`,
+        });
+    }
+
+    return createInertiaApp({
         page,
         render: renderToString,
         resolve: (name) =>
@@ -17,7 +29,7 @@ createServer((page) =>
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
-                .use(ZiggyVue);
+                .use(ZiggyVue, Ziggy);
         },
-    }),
-);
+    });
+});
