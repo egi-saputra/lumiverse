721ed8419d39205ceca83d679065a22835f5d90a
egi-saputra <hanalikaproject@gmail.com>
2026-09-10 20:39:38 +0700

    P

--- routes/console.php
+++ routes/console.php
@@ -11,9 +11,9 @@ Schedule::command('subscriptions:revert-expired-to-free')->hourly();
 Schedule::command('subscriptions:cleanup-excess-users')->dailyAt('02:00');
 Schedule::command('ai-invoices:expire-stale')->everyFifteenMinutes();
 Schedule::command('sitemap:generate')->daily();
-Schedule::command('tenants:prune-journals')->twiceYearlyOn(1, 7, '00:05');
-Schedule::command('tenants:prune-absensi')->twiceYearlyOn(1, 7, '00:10');
-Schedule::command('tenants:reset-ujian')->twiceYearlyOn(1, 7, '00:15');
+Schedule::command('tenants:prune-journals')->cron('5 0 1 1,7 *');
+Schedule::command('tenants:prune-absensi')->cron('10 0 1 1,7 *');
+Schedule::command('tenants:reset-ujian')->cron('15 0 1 1,7 *');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
