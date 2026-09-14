e1799bb4a44fa884d74614581a41309f1dc1d6e0
egi-saputra <hanalikaproject@gmail.com>
2026-09-14 20:30:57 +0700

    P

--- app/Models/RiwayatUjian.php
+++ app/Models/RiwayatUjian.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RiwayatUjian extends Model
 {
+    use SoftDeletes;
     protected $table = 'riwayat_ujian';
 
     protected $fillable = [
